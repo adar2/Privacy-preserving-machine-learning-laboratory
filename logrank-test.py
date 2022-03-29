7d7f84169212f3fb70f1bc93bfbef700f67512f2
Adar Warmund <adar.warmund@intel.com>
2022-03-29 19:44:23 +0300

    logrank test fix

--- logrank-test.py
+++ logrank-test.py
@@ -1,10 +1,7 @@
 import math
 
-import numpy as np
 import pandas as pd
 
-
-# import matplotlib.pyplot as plt
 
 class LogRankTest:
     def __init__(self, data: pd.DataFrame):
@@ -16,26 +13,24 @@ class LogRankTest:
 
     def __get_subjects_at_risk(self, time, group=None):
         if group is None:
-            return len(self.df[(df.time >= time) & (df.Died == 0)])
-        return len(self.df[(df.time >= time) & (df.Died == 0) & (df.group == group)])
+            return len(self.df[df.time >= time])
+        return len(self.df[(df.time >= time) & (df.group == group)])
 
-    def __get_number_of_failures(self, time, group):
+    def __get_number_of_failures(self, time, group=None):
+        if group is None:
+            return len(self.df[(df.time == time) & (df.Died == 1)])
         return len(self.df[(df.time == time) & (df.Died == 1) & (df.group == group)])
 
     @staticmethod
-    def __hyper_geometric(num_of_subjects, num_of_subjects_in_group, num_of_failures):
-        return np.random.hypergeometric(num_of_subjects, num_of_failures, num_of_subjects_in_group)
-
-    @staticmethod
-    def __hyper_geometric_expectation(num_of_subjects, num_of_subjects_in_group, num_of_failures):
+    def __expectation(num_of_subjects, num_of_subjects_in_group, num_of_failures):
         return (num_of_subjects_in_group / num_of_subjects) * num_of_failures
 
     @staticmethod
-    def __hyper_geometric_variance(num_of_subjects, num_of_subjects_in_group, num_of_failures):
+    def __variance(num_of_subjects, num_of_subjects_in_group, num_of_failures):
         if num_of_subjects == 1:
-            num_of_subjects += 1
-        expected_value = LogRankTest.__hyper_geometric_expectation(num_of_subjects, num_of_subjects_in_group,
-                                                                   num_of_failures)
+            return 0
+        expected_value = LogRankTest.__expectation(num_of_subjects, num_of_subjects_in_group,
+                                                   num_of_failures)
         return expected_value * ((num_of_subjects - num_of_failures) / num_of_subjects) * (
                 (num_of_subjects - num_of_subjects_in_group) / (num_of_subjects - 1))
 
@@ -48,20 +43,21 @@ class LogRankTest:
         V_b = 0
         for time in self.__get_distinct_time():
             subjects_at_risk = self.__get_subjects_at_risk(time)
+            num_of_failures = self.__get_number_of_failures(time)
             for group in [1, 2]:
                 group_subjects_at_risk = self.__get_subjects_at_risk(time, group)
-                num_of_failures = self.__get_number_of_failures(time, group)
-                if group_subjects_at_risk <= 0:
-                    continue
-                o = self.__hyper_geometric(subjects_at_risk, group_subjects_at_risk, num_of_failures)
+                num_of_failures_in_group = self.__get_number_of_failures(time, group)
+                e = self.__expectation(subjects_at_risk, group_subjects_at_risk, num_of_failures)
+                v = self.__variance(subjects_at_risk, group_subjects_at_risk, num_of_failures)
+                o = num_of_failures_in_group
                 if group == 1:
                     O_a += o
-                    E_a += self.__hyper_geometric_expectation(subjects_at_risk, group_subjects_at_risk, o)
-                    V_a += self.__hyper_geometric_variance(subjects_at_risk, group_subjects_at_risk, o)
+                    E_a += e
+                    V_a += v
                 else:
                     O_b += o
-                    E_b += self.__hyper_geometric_expectation(subjects_at_risk, group_subjects_at_risk, o)
-                    V_b += self.__hyper_geometric_variance(subjects_at_risk, group_subjects_at_risk, o)
+                    E_b += e
+                    V_b += v
         Z_a = (O_a - E_a) / math.sqrt(V_a)
         Z_b = (O_b - E_b) / math.sqrt(V_b)
         return (Z_a, Z_b)
@@ -70,4 +66,13 @@ class LogRankTest:
 if __name__ == '__main__':
     df = pd.read_csv('test-data.txt', sep='\t')
     l = LogRankTest(df)
-    print(l.test())
+    res = l.test()
+    print(f'group A Z value :{res[0]}, group B Z value:{res[1]}, their sum {res[0] + res[1]}')
+    # print()
+    # T1 = list(df[df.group == 1].time)
+    # T2 = list(df[df.group == 2].time)
+    # E1 = list(df[df.group == 1].Died)
+    # E2 = list(df[df.group == 2].Died)
+    #
+    # results = logrank_test(T1, T2, event_observed_A=E1, event_observed_B=E2)
+    # results.ascii_print()
