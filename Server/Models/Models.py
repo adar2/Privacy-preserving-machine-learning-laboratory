7de9f072ed63e88c4d86264356e05d8922663171
Adar Warmund <adar.warmund@intel.com>
2022-05-01 14:46:13 +0300

    added client side calculations

--- Server/Models/Models.py
+++ Server/Models/Models.py
@@ -1,19 +1,32 @@
+import uuid
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
 
 db = SQLAlchemy()
 
 
 class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     param_1 = db.Column(db.String(200), nullable=False)
     param_2 = db.Column(db.String(200), nullable=False)
-    # experiment_id = db.Column(db.String, db.ForeignKey('experiments.id'))
-    experiment = db.relationship('Category', backref=db.backref('messages', lazy=True))
-    experiment_id = db.Column(db.String, db.ForeignKey('experiment.id'), nullable=False)
+    experiment = db.relationship('Experiment', backref=db.backref('messages', lazy=True))
+    experiment_id = db.Column(UUID(as_uuid=True), db.ForeignKey('experiment.id'), nullable=False)
 
 
 class Experiment(db.Model):
-    # __tablename__ = 'experiments'
-    id = db.Column(db.String(200), primary_key=True, nullable=False)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(200), nullable=False)
-    # messages = db.relationship('Message', backref='experiment', lazy='dynamic')
+
+
+def add_experiment(experiment: Experiment) -> None:
+    db.session.add(experiment)
+    db.session.commit()
+
+
+def get_experiment_by_id(uid) -> Experiment:
+    return Experiment.query.filter_by(id=uid).first()
+
+
+def add_message(message: Message) -> None:
+    db.session.add(message)
+    db.session.commit()
--- Server/app.py
+++ Server/app.py
@@ -1,39 +1,57 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import secrets
+from flask import Flask, render_template, request, abort,session,url_for
+from Models.Models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+app.secret_key = secrets.token_bytes(32)
+db.init_app(app)
 
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    param_1 = db.Column(db.String(200), nullable=False)
-    param_2 = db.Column(db.String(200), nullable=False)
-    experiment = db.relationship('Experiment', backref=db.backref('messages', lazy=True))
-    experiment_id = db.Column(db.Integer, db.ForeignKey('experiment.id',nullable=False))
+def is_valid_uuid(uuid_to_test):
+    try:
+        uuid_obj = UUID(uuid_to_test)
+    except ValueError:
+        return False
+    return uuid_obj.as_uuid == uuid_to_test
 
 
-class Experiment(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(200), nullable=False)
-    # messages = db.relationship('Message', backref='experiment', lazy='dynamic')
+@app.route('/submit-results',methods=['POST'])
+def submit_results():
+    try:
+        experiment_id = session["eid"]
+        print(request.data)
+        return render_template('index.html')
+    except Exception as e:
+        print(e)
+        abort(400)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     try:
-        ex = Experiment(id=1, name='fuck this shit')
-        m = Message(experiment=ex, param_1='daaa', param_2='dddd')
-        # ex.messages.add(m)
-        db.session.add(ex)
-        db.session.add(m)
-        db.session.commit()
-        return 'Ok'
+        if request.method == 'POST':
+            if request.form['action'] is None:
+                abort(404)
+            action = request.form['action']
+            if action == 'join_experiment':
+                uid = request.form['experiment_id']
+                if not is_valid_uuid(uid):
+                    return 'id is not valid'
+                ex = Experiment.query.filter_by(id=uid).first()
+                if ex is not None:
+                    return render_template('experiment.html', name=ex.name, id=ex.id)
+                return "Id doesn't exists"
+            if action == 'create_experiment':
+                ex = Experiment(name=request.form['experiment_name'])
+                add_experiment(ex)
+                session["eid"] = ex.id
+                return render_template('experiment.html',name=ex.name,id=ex.id)
+        return render_template('index.html')
     except Exception as e:
         print(e)
         return 'There was an issue adding your task'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
