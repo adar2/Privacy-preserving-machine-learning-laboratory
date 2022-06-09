452e29a730c66b079c1bc5371fb21e1c48251c46
Ronnie <ronnieshafran@gmail.com>
2022-06-09 19:42:39 +0300

    After functions

--- Client/regenerate_ui.bat
+++ Client/regenerate_ui.bat
@@ -1,6 +1,5 @@
  pyuic5 -o UI/PyFiles/NewExperimentDialog.py UI/UiFiles/NewExperimentDialog.ui
  pyuic5 -o UI/PyFiles/UploadDataDialog.py UI/UiFiles/UploadDataDialog.ui
  pyuic5 -o UI/PyFiles/ResultsViewDialog.py UI/UiFiles/ResultsViewDialog.ui
- pyuic5 -o UI/PyFiles/LocalAnalysisDialog.py UI/UiFiles/LocalAnalysisDialog.ui
  pyuic5 -o UI/PyFiles/SimulationsDialog.py UI/UiFiles/SimulationsDialog.ui
  pyuic5 -o UI/PyFiles/MainWindow.py UI/UiFiles/MainWindow.ui
